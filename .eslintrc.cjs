dcf8881f299b88c9043d20e7d95f556cb6511f92
Exo Mercado <jexodusmercado@gmail.com>
2024-03-18 07:37:49 +0800

    Revert "fix(eslint): updated eslint for vite recommendations"
    This reverts commit 4ac2aee5ccb6c5563161e1b2d115384b3c05fca7.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,7 @@ module.exports = {
     env: { browser: true, es2020: true },
     extends: [
         'eslint:recommended',
-        'plugin:@typescript-eslint/recommended-type-checked',
-        '@typescript-eslint/stylistic-type-checked',
-        'plugin:react/recommended',
-        'plugin:react/jsx-runtime',
+        'plugin:@typescript-eslint/recommended',
         'plugin:react-hooks/recommended',
         'plugin:@tanstack/eslint-plugin-query/recommended',
         'prettier',
